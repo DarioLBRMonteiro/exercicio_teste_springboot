0081cdbe073d3782c741b6f6579f4f6585c4f529
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-06-15 19:05:55 -0300

    Service layer, component registration

--- src/main/java/com/dlbrmonteiro/cursospringboot/repositories/UserRepository.java
+++ src/main/java/com/dlbrmonteiro/cursospringboot/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.dlbrmonteiro.cursospringboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dlbrmonteiro.cursospringboot.entities.User;
 
+@Repository //opcional
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
