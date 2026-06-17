c82173a5be19446078f15f475d572a902df362f0
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-06-16 23:38:22 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/dlbrmonteiro/cursospringboot/entities/Product.java
+++ src/main/java/com/dlbrmonteiro/cursospringboot/entities/Product.java
@@ -6,11 +6,14 @@ import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import jakarta.persistence.UniqueConstraint;
 
 @Entity
@@ -26,7 +29,12 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", schema = "curso", 
+	joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"),
+	uniqueConstraints = @UniqueConstraint(name = "pkProductCategory",columnNames = {"product_id","category_id"}),
+	foreignKey = @ForeignKey(name = "fkProductCategoryProduct"),
+	inverseForeignKey = @ForeignKey(name = "fkProductCategoryCategory"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {		
