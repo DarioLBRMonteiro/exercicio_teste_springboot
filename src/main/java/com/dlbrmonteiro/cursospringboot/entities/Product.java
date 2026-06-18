a2425cee264ea08e5bd2a1db492bfb6941661287
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-06-17 22:58:36 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/dlbrmonteiro/cursospringboot/entities/Product.java
+++ src/main/java/com/dlbrmonteiro/cursospringboot/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 
@@ -36,6 +39,9 @@ public class Product implements Serializable{
 	foreignKey = @ForeignKey(name = "fkProductCategoryProduct"),
 	inverseForeignKey = @ForeignKey(name = "fkProductCategoryCategory"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {		
 	}
@@ -91,6 +97,15 @@ public class Product implements Serializable{
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
