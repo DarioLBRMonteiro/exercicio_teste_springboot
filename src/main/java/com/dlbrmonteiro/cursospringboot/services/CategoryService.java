b30d79da2323dc1a5773774896fb09c6219199dc
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-06-23 18:25:55 -0300

    crud - product and category

--- src/main/java/com/dlbrmonteiro/cursospringboot/services/CategoryService.java
+++ src/main/java/com/dlbrmonteiro/cursospringboot/services/CategoryService.java
@@ -1,29 +1,68 @@
 package com.dlbrmonteiro.cursospringboot.services;
 
 import java.util.List;
-//import java.util.Optional;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dlbrmonteiro.cursospringboot.entities.Category;
 import com.dlbrmonteiro.cursospringboot.repositories.CategoryRepository;
+import com.dlbrmonteiro.cursospringboot.services.exceptions.DatabaseException;
+import com.dlbrmonteiro.cursospringboot.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
-	public List<Category> findAll(){
+	public List<Category> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Category findById(Long id) {
-		//Optional<Category> obj = repository.findById(id);
+		// Optional<Category> obj = repository.findById(id);
 		// return obj.get();
-		
-		Category category = repository.findById(id).orElseThrow();
-		return category;
+
+//		Category category = repository.findById(id).orElseThrow();
+//		return category;
+
+		Optional<Category> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public Category insert(Category obj) {
+		return repository.save(obj); // codigo de status http 200
+	}
+
+	public void delete(Long id) {
+		try {
+//			if (!repository.existsById(id))
+//				throw new ResourceNotFoundException(id);
+			if (findById(id) != null)
+				repository.deleteById(id);
+		} catch (ResourceNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Category update(Long id, Category category) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			updateData(entity, category);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 }
--- src/main/java/com/dlbrmonteiro/cursospringboot/services/ProductService.java
+++ src/main/java/com/dlbrmonteiro/cursospringboot/services/ProductService.java
@@ -2,12 +2,18 @@ package com.dlbrmonteiro.cursospringboot.services;
 
 import java.util.List;
 //import java.util.Optional;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dlbrmonteiro.cursospringboot.entities.Product;
 import com.dlbrmonteiro.cursospringboot.repositories.ProductRepository;
+import com.dlbrmonteiro.cursospringboot.services.exceptions.DatabaseException;
+import com.dlbrmonteiro.cursospringboot.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
@@ -23,7 +29,43 @@ public class ProductService {
 		//Optional<Product> obj = repository.findById(id);
 		// return obj.get();
 		
-		Product product = repository.findById(id).orElseThrow();
-		return product;
+//		Product product = repository.findById(id).orElseThrow();
+//		return product;
+		
+		Optional<Product> obj = repository.findById(id);	
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));						
 	}
+	
+	public Product insert(Product obj) {
+		return repository.save(obj); // codigo de status http 200
+	}
+	
+	public void delete(Long id) {
+		try {
+//			if (!repository.existsById(id))
+//				throw new ResourceNotFoundException(id);
+			if (findById(id) != null) 
+				repository.deleteById(id);
+		} catch (ResourceNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Product update(Long id, Product product) {
+		try {
+			Product entity = repository.getReferenceById(id);
+			updateData(entity,product);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}		
+	}
+	
+	private void updateData(Product entity, Product obj) {
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setImgUrl(obj.getImgUrl());
+	}	
 }
